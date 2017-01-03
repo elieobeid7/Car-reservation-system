67a17ef80716b5077f134975625ac07c57e6f8ea
Elie Obeid <Elie Obeid>
2017-01-03 21:39:04 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+
+
+
+
+<script type="text/javascript">
+	$(document).ready( function() {
+    $('#datepicker').val(new Date().toDateInputValue());
+});​
+</script>
+<script>
+function myFunction() {
+    alert("Thank you, a message has been sent to the admin");
+}
+</script>
+
+        <script type="text/javascript">
+            $('#timepicker1').timepicker();
+        </script>
+
+
+</html>
